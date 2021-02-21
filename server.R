0b24557853fab578eb6ddb14e2a6392c7fe6b011
VVeiZhang <34899541+vveizhang@users.noreply.github.com>
2021-02-20 23:51:39 -0600

    Add files via upload

---
+++ server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(ggplot2)
+
+
+MAD <- read.csv('MEageMedianMAD.csv')
+
+colours <-  c('#FF0000',"#CC00CC","#CC6600","#9933FF",'#00CCCC',"#0000CC")
+
+function(input, output) {
+  output$plot1 <- renderPlot({MAD %>% 
+    ggplot() + 
+      geom_line(aes(MAD[,5], MAD[,as.integer(input$var) + 5]), group = 1,color = sample(colours,1), size = 1) +
+      geom_ribbon(aes(x = MAD[,5],ymax = (MAD[,as.integer(input$var) + 5] + MAD[,as.integer(input$var) + 27]), 
+                      ymin = (MAD[,as.integer(input$var) + 5] - MAD[,as.integer(input$var) + 27])),group = 1, alpha = 0.5,fill = "#FFCCCC") +
+      xlim(2,12) + xlab("log2PCW") + ylab(paste("Module",toString(input$var)," ",'Eigengene  Expression')) + 
+      geom_vline(xintercept = 5.3)
+  })
+}
---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(ggplot2)
+
+shinyUI(fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a gene module to display",
+                  choices = (1:22),
+                  selected = 1)),
+    
+    mainPanel(
+      plotOutput("plot1")
+    )
+  )
+))
